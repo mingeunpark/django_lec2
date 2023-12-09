3c0aa44b841f1970e6e9abd94b991749f4f22dd4
MinGeun <sksjge9222@gmail.com>
2023-12-09 16:11:58 +0900

    Main Page 생성

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from django.shortcuts import render
+
+
+def mainpage(request):
+    return render(request, 'pages/mainpage.html')
+
+
+def company(request):
+    return render(request, 'pages/company_info_html')
